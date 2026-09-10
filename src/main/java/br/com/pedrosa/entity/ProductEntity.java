78f5a131acfa8af695450519168527a1a31ddc87
fabiopedrosa1980 <bito.pedrosa@gmail.com>
2026-09-10 18:14:02 -0300

    Ajustes importacao de dados

--- src/main/java/br/com/pedrosa/entity/ProductEntity.java
+++ src/main/java/br/com/pedrosa/entity/ProductEntity.java
@@ -3,10 +3,12 @@ package br.com.pedrosa.entity;
 import br.com.pedrosa.request.ProductRequest;
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 import java.math.BigDecimal;
 
 @Entity
+@Table(name = "Product")
 public class ProductEntity extends PanacheEntity {
     public String name;
     public BigDecimal price;
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,6 +4,6 @@ CREATE TABLE Product (
     price DECIMAL(10, 2) NOT NULL
 );
 
-INSERT INTO Product(id, name,price) VALUES (1, 'Mackbock',23000.00);
-INSERT INTO Product(id, name,price) VALUES (2, 'Iphone 18 Pro Max',13000.00);
+INSERT INTO Product(id, name,price) VALUES (1, 'Mackbock Pro',23000.00);
+INSERT INTO Product(id, name,price) VALUES (2, 'Iphone 18 Pro Max 1TB',13000.00);
 INSERT INTO Product(id, name,price) VALUES (3, 'Ipod',5000.00);
